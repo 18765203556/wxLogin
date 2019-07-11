af1246c88e98c5984d63b208ec0f06ccc4b54bd9
zhudingding <zhudingding@82614b60-80d4-46e0-80e4-af02a555b640>
2019-07-11 08:01:02 +0000

    部分代码提交

--- trunk/sourceCode/boot-security/src/main/java/com/boot/security/server/controller/WeChatController.java
+++ trunk/sourceCode/boot-security/src/main/java/com/boot/security/server/controller/WeChatController.java
@@ -1,5 +1,6 @@
 package com.boot.security.server.controller;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,6 +24,8 @@ import com.boot.security.server.model.TComment;
 import com.boot.security.server.model.TCompany;
 import com.boot.security.server.model.TDynamic;
 import com.boot.security.server.model.TEmploy;
+import com.boot.security.server.model.TEmployCollect;
+import com.boot.security.server.model.TEmployDeliver;
 import com.boot.security.server.page.table.PageTableRequest;
 import com.boot.security.server.page.table.PageTableResponse;
 import com.boot.security.server.service.WeChatService;
@@ -164,8 +167,9 @@ public class WeChatController extends BaseController{
 			//获取到的用户id
 			String userId=(String)request.getAttribute("userId");
 			entity.setCreateUser(userId);
+			entity.setCreateTime(new Date());
 			 weChatService.saveDynamic(entity);
-			 return success(null);
+			 return success("保存成功");
 		} catch (Exception e) {
 			e.printStackTrace();
 			 return fail("系统异常请联系管理员！");
@@ -202,10 +206,12 @@ public class WeChatController extends BaseController{
 			entity.setCommentUser(userId);
 			//创建人
 			entity.setCreateUser(userId);
+			//创建时间
+			entity.setCreateTime(new Date());
 			//状态字段
 			entity.setStatus(data.getString("status"));
 			 weChatService.saveComment(entity);
-			 return success(null);
+			 return success("保存成功!");
 		} catch (Exception e) {
 			e.printStackTrace();
 			 return fail("系统异常请联系管理员！");
@@ -302,10 +308,15 @@ public class WeChatController extends BaseController{
 	 */
 	@GetMapping("/listEmploy")
     @ApiOperation(value = "工作筛选")
-    public String listEmploy(PageTableRequest request,Integer pageSize,Integer page) {
+    public String listEmploy(HttpServletRequest req,PageTableRequest request,Integer pageSize,Integer page) {
 		List<TEmploy> dataList=null;
 		PageTableResponse response;
 		try {
+			String userId=(String)req.getAttribute("userId");
+			Map<String, Object> params=request.getParams();
+			if(params!=null){
+				params.put("userId", userId);
+			}
 			if(pageSize!=null && page!=null){
 				Integer offset=(page-1)*pageSize;
 				Integer limit=page*pageSize;
@@ -320,9 +331,9 @@ public class WeChatController extends BaseController{
 					totalCount=totalCount+dataList.size();
 				}
 				resultMap.put("totalCount", totalCount);
-				resultMap.put("city", dataList);
+				resultMap.put("list", dataList);
 				log.info(JSON.toJSONString(resultMap));
-				return JSON.toJSONString(resultMap);
+				return success(resultMap);
 			}else{
 				return fail("分页参数不正确，请核实参数");
 			}
@@ -358,5 +369,292 @@ public class WeChatController extends BaseController{
 			 return fail("系统异常请联系管理员！");
 		}
        
+    }
+	/**
+	 * 	收藏工作
+	 *  Description:
+	 *  @author xiaoding  DateTime 2019年7月10日 上午9:07:23
+	 *  @param json
+	 *  @param request
+	 *  @return
+	 
+	 */
+	
+	@PostMapping("/saveCollect")
+    @ApiOperation(value = "收藏工作")
+    public String saveCollect(@RequestBody String json,HttpServletRequest request) {
+		try {
+			JSONObject data=JSONObject.parseObject(json);
+			TEmployCollect entity=new TEmployCollect();
+			//获取id
+			entity.setId(UUID.randomUUID().toString().replaceAll("-", ""));
+			//获取到的用户id
+			String userId=(String)request.getAttribute("userId");
+			String employId=data.getString("employId");
+			entity.setEmployId(employId);
+			//评论用户
+			entity.setCusSelfId(userId);
+			//创建人
+			entity.setCreateUser(userId);
+			//创建时间
+			entity.setCreateTime(new Date());
+			//状态字段
+			String result= weChatService.saveCollect(entity);
+			return success(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+			 return fail("系统异常请联系管理员！");
+		}
+    }
+	/**
+	 *  查看我的收藏
+	 *  Description:
+	 *  @author xiaoding  DateTime 2019年7月10日 下午2:28:01
+	 *  @param request
+	 *  @param pageSize
+	 *  @param page
+	 *  @return
+	 
+	 */
+	@GetMapping("/listCollect")
+    @ApiOperation(value = "查看我的收藏")
+    public String listCollect(HttpServletRequest req,PageTableRequest request,Integer pageSize,Integer page) {
+		List<TEmploy> dataList=null;
+		PageTableResponse response;
+		try {
+			String userId=(String)req.getAttribute("userId");
+			Map<String, Object> params=request.getParams();
+			if(params!=null){
+				params.put("cusSelfId", userId);
+//				params.put("tableName", "t_employ_collect");
+			}
+			if(pageSize!=null && page!=null){
+				Integer offset=(page-1)*pageSize;
+				Integer limit=page*pageSize;
+				request.setOffset(offset);
+				request.setLimit(limit);
+				
+				Map<String,Object> resultMap = new HashMap<String,Object>();
+				response = weChatService.listCollect(request);
+				dataList=(List<TEmploy>)response.getData();
+				int totalCount=0;
+				if(dataList!=null){
+					totalCount=totalCount+dataList.size();
+				}
+				resultMap.put("totalCount", totalCount);
+				resultMap.put("list", dataList);
+				log.info(JSON.toJSONString(resultMap));
+				return success(resultMap);
+			}else{
+				return fail("分页参数不正确，请核实参数");
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return fail("系统出错了请联系管理员");
+		}
+		
+    }
+	
+	/**
+	 * 	投递工作
+	 *  Description:
+	 *  @author xiaoding  DateTime 2019年7月10日 上午9:07:23
+	 *  @param json
+	 *  @param request
+	 *  @return
+	 
+	 */
+	
+	@PostMapping("/saveDeliver")
+    @ApiOperation(value = "投递工作")
+    public String saveDeliver(@RequestBody String json,HttpServletRequest request) {
+		try {
+			JSONObject data=JSONObject.parseObject(json);
+			TEmployDeliver entity=new TEmployDeliver();
+			//获取id
+			entity.setId(UUID.randomUUID().toString().replaceAll("-", ""));
+			//获取到的用户id
+			String userId=(String)request.getAttribute("userId");
+			String employId=data.getString("employId");
+			entity.setEmployId(employId);
+			//评论用户
+			entity.setCusSelfId(userId);
+			//创建人
+			entity.setCreateUser(userId);
+			//创建时间
+			entity.setCreateTime(new Date());
+			//状态字段
+			String result= weChatService.saveDeliver(entity);
+			return success(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+			 return fail("系统异常请联系管理员！");
+		}
+    }
+	/**
+	 *  查看我的投递
+	 *  Description:
+	 *  @author xiaoding  DateTime 2019年7月10日 下午2:28:01
+	 *  @param request
+	 *  @param pageSize
+	 *  @param page
+	 *  @return
+	 
+	 */
+	@GetMapping("/listDeliver")
+    @ApiOperation(value = "查看我的投递")
+    public String listDeliver(HttpServletRequest req,PageTableRequest request,Integer pageSize,Integer page) {
+		List<TEmploy> dataList=null;
+		PageTableResponse response;
+		try {
+			String userId=(String)req.getAttribute("userId");
+			Map<String, Object> params=request.getParams();
+			if(params!=null){
+				params.put("cusSelfId", userId);
+//				params.put("tableName", "t_employ_deliver");
+			}
+			if(pageSize!=null && page!=null){
+				Integer offset=(page-1)*pageSize;
+				Integer limit=page*pageSize;
+				request.setOffset(offset);
+				request.setLimit(limit);
+				
+				Map<String,Object> resultMap = new HashMap<String,Object>();
+				response = weChatService.listCollect(request);
+				dataList=(List<TEmploy>)response.getData();
+				int totalCount=0;
+				if(dataList!=null){
+					totalCount=totalCount+dataList.size();
+				}
+				resultMap.put("totalCount", totalCount);
+				resultMap.put("list", dataList);
+				log.info(JSON.toJSONString(resultMap));
+				return success(resultMap);
+			}else{
+				return fail("分页参数不正确，请核实参数");
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return fail("系统出错了请联系管理员");
+		}
+		
+    }
+	/**
+	 * 	取消收藏
+	 *  Description:
+	 *  @author xiaoding  DateTime 2019年7月10日 上午9:07:23
+	 *  @param json
+	 *  @param request
+	 *  @return
+	 
+	 */
+	
+	@GetMapping("/cancelCollect")
+    @ApiOperation(value = "取消收藏")
+    public String cancelCollect(String id,HttpServletRequest request) {
+		try {
+			String userId=(String)request.getAttribute("userId");
+			String result=weChatService.cancelCollect(id,userId);
+			return success(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+			 return fail("系统异常请联系管理员！");
+		}
+    }
+	/**
+	 *  查看我的我点赞的动态
+	 *  Description:
+	 *  @author xiaoding  DateTime 2019年7月10日 下午2:28:01
+	 *  @param request
+	 *  @param pageSize
+	 *  @param page
+	 *  @return
+	 
+	 */
+	@GetMapping("/listAllGiveUp")
+    @ApiOperation(value = "查看我的我点赞的动态")
+    public String listAllGiveUp(HttpServletRequest req,PageTableRequest request,Integer pageSize,Integer page) {
+		List<TEmploy> dataList=null;
+		PageTableResponse response;
+		try {
+			String userId=(String)req.getAttribute("userId");
+			Map<String, Object> params=request.getParams();
+			if(params!=null){
+				params.put("userId", userId);
+			}
+			if(pageSize!=null && page!=null){
+				Integer offset=(page-1)*pageSize;
+				Integer limit=page*pageSize;
+				request.setOffset(offset);
+				request.setLimit(limit);
+				
+				Map<String,Object> resultMap = new HashMap<String,Object>();
+				response = weChatService.listAllGiveUp(request);
+				dataList=(List<TEmploy>)response.getData();
+				int totalCount=0;
+				if(dataList!=null){
+					totalCount=totalCount+dataList.size();
+				}
+				resultMap.put("totalCount", totalCount);
+				resultMap.put("list", dataList);
+				log.info(JSON.toJSONString(resultMap));
+				return success(resultMap);
+			}else{
+				return fail("分页参数不正确，请核实参数");
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return fail("系统出错了请联系管理员");
+		}
+		
+    }
+	/**
+	 * 我发布的动态
+	 *  Description:
+	 *  @author xiaoding  DateTime 2019年7月10日 下午2:28:01
+	 *  @param request
+	 *  @param pageSize
+	 *  @param page
+	 *  @return
+	 
+	 */
+	@GetMapping("/listDynamicByMe")
+    @ApiOperation(value = "我发布的动态")
+    public String listDynamicByMe(HttpServletRequest req,PageTableRequest request,Integer pageSize,Integer page) {
+		List<TDynamic> dataList=null;
+		PageTableResponse response;
+		try {
+			String userId=(String)req.getAttribute("userId");
+			Map<String, Object> params=request.getParams();
+			if(params!=null){
+				params.put("userId", userId);
+			}
+			if(pageSize!=null && page!=null){
+				Integer offset=(page-1)*pageSize;
+				Integer limit=page*pageSize;
+				request.setOffset(offset);
+				request.setLimit(limit);
+			}else{
+				return fail("分页参数不正确，请核实参数");
+			}
+			Map<String,Object> resultMap = new HashMap<String,Object>();
+			response = weChatService.listDynamic(request);
+			dataList=(List<TDynamic>)response.getData();
+			int totalCount=0;
+			if(dataList!=null){
+				totalCount=totalCount+dataList.size();
+			}
+			resultMap.put("totalCount", totalCount);
+			resultMap.put("list", dataList);
+			log.info(JSON.toJSONString(resultMap));
+			return success(resultMap);
+		} catch (Exception e) {
+			e.printStackTrace();
+			log.info(JSON.toJSONString(e));
+			 return fail("系统异常请联系管理员！");
+		}
     }
 }
